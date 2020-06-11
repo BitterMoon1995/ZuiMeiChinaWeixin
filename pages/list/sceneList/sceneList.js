dca02f987b6a43dfb246c6b0cb6aea208a70558a
BitterMoon1995 <1559069144@qq.com>
2020-06-11 17:27:02 +0800

    George · BlackNigger , R.N.G

--- pages/list/sceneList/sceneList.js
+++ pages/list/sceneList/sceneList.js
@@ -1,66 +1,112 @@
-// pages/list/sceneList.js
+import {
+    request
+} from "../../../request/index"
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        showScenes: true,
+        sceneList: [],
 
-  },
+        condition:'',
+        resultList: []
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    getShowList() {
+        request({
+            url: 'http://localhost:2020/mini/scene/showList'
+        })
+        .then(result => {
+            console.log(result)
+            this.setData({
+                sceneList: result.data
+            })
+        })
+    },
 
-  },
+    handleInput(e) {
+        if (e.detail.value!=='')
+        this.setData({
+            showScenes: false,
+            condition: e.detail.value
+        })
+        else
+        this.setData({
+            showScenes: true
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    search() {
+        if (!this.data.condition.trim()) return
+        request({
+            url: 'http://localhost:2020/mini/search/overall',
+            data: {
+                condition: this.data.condition
+            }
+        })
+        .then(result => {
+            this.setData({
+                resultList: result.data
+            })
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.getShowList()
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  }
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,95 +1,105 @@
 import {
-  request
+    request
 } from "../../request/index.js"
 // pages/search/search.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    condition:'',
-    type:2,
-    resultList:[]
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        condition: '',
+        type: 8888,
 
-  handleInput(e){
-    this.setData({
-      condition:e.detail.value
-    })
-  },
+        resultList: []
+    },
 
-  search() {
-    if (this.data.type===2){
-      if (!this.data.condition.trim()) return
-      request({
-        url:'http://localhost:2020/mini/scene/search',
-        data:{
-          condition:this.data.condition
-        }
-      })
-      .then(result=>{
-        console.log(result.data)
+    handleInput(e) {
         this.setData({
-          resultList:result.data
+            condition: e.detail.value
         })
-      })
+    },
+
+    search() {
+        let tag = this.data.type
+        /*
+        javaScript中两个等于号和三个等于号之间的区别：
+        ==先转换类型再比较，===先判断类型，如果不是同一类型直接为false
+         */
+        if (tag == 0) {
+            this.searchAll()
+        }
+    },
+
+    searchAll() {
+        if (!this.data.condition.trim()) return
+        request({
+            url: 'http://localhost:2020/mini/search/overall',
+            data: {
+                condition: this.data.condition
+            }
+        })
+        .then(result => {
+            this.setData({
+                resultList: result.data
+            })
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.setData({
+            type:options.type
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
     }
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
