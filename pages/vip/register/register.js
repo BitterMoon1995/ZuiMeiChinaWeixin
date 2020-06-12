d6f95c81fac02210dfbb7ae79de566fbbd8e99c9
BitterMoon1995 <1559069144@qq.com>
2020-06-12 18:12:05 +0800

    ”佛跳墙“

--- pages/vip/register/register.js
+++ pages/vip/register/register.js
@@ -1,144 +1,151 @@
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    phone:'',
-    real_name:'',
-    imgs: [],
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        phone: '',
+        realName: '',
+        idNum:'',
 
-
-
-    hide:false,
-  },
-  // 上传图片
-  chooseImg: function (e) {
-    let that = this;
-    let imgs = this.data.imgs;
-    if (imgs.length >= 9) {
-      this.setData({
-        lenMore: 1
-      });
-      setTimeout(function () {
-        that.setData({
-          lenMore: 0
-        });
-      }, 2500);
-      return false;
-    }
-    wx.chooseImage({
-      count: 1, // 默认9
-      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-      success: function (res) {
-        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        let tempFilePaths = res.tempFilePaths;
-        that.setData({hide: true})
-        let imgs = that.data.imgs
-
-        // console.log(tempFilePaths + '----');
-        for (let i = 0; i < tempFilePaths.length; i++) {
-          if (imgs.length >= 9) {
-            that.setData({
-              imgs: imgs
+        imgs: [],
+        hide: false,
+    },
+    phone(e) {
+        this.setData({phone:e.detail.value})
+    },
+    realName(e){
+        this.setData({real_name:e.detail.value})
+    },
+    idNum(e){
+        this.setData({idNum:e.detail.value})
+    },
+    // 上传图片
+    chooseImg: function (e) {
+        let that = this;
+        let imgs = this.data.imgs;
+        if (imgs.length >= 9) {
+            this.setData({
+                lenMore: 1
             });
+            setTimeout(function () {
+                that.setData({
+                    lenMore: 0
+                });
+            }, 2500);
             return false;
-          } else {
-            imgs.push(tempFilePaths[i]);
-          }
         }
-        // console.log(imgs);
-        that.setData({
-          imgs: imgs
+        wx.chooseImage({
+            count: 1, // 默认9
+            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+            success: function (res) {
+                // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+                let tempFilePaths = res.tempFilePaths;
+                that.setData({hide: true})
+                let imgs = that.data.imgs
+
+                // console.log(tempFilePaths + '----');
+                for (let i = 0; i < tempFilePaths.length; i++) {
+                    if (imgs.length >= 9) {
+                        that.setData({
+                            imgs: imgs
+                        });
+                        return false;
+                    } else {
+                        imgs.push(tempFilePaths[i]);
+                    }
+                }
+                // console.log(imgs);
+                that.setData({
+                    imgs: imgs
+                });
+            }
         });
-      }
-    });
-  },
-  // 删除图片
-  deleteImg: function (e) {
-    let imgs = this.data.imgs;
-    let index = e.currentTarget.dataset.index;
-    imgs.splice(index, 1);
-    this.setData({
-      imgs: imgs,
-      hide: false
-    });
-  },
-  // 预览图片
-  previewImg: function (e) {
-    //获取当前图片的下标
-    let index = e.currentTarget.dataset.index;
-    //所有图片
-    let imgs = this.data.imgs;
-    wx.previewImage({
-      //当前显示图片
-      current: imgs[index],
-      //所有图片
-      urls: imgs
-    })
-  },
+    },
+    // 删除图片
+    deleteImg: function (e) {
+        let imgs = this.data.imgs;
+        let index = e.currentTarget.dataset.index;
+        imgs.splice(index, 1);
+        this.setData({
+            imgs: imgs,
+            hide: false
+        });
+    },
+    // 预览图片
+    previewImg: function (e) {
+        //获取当前图片的下标
+        let index = e.currentTarget.dataset.index;
+        //所有图片
+        let imgs = this.data.imgs;
+        wx.previewImage({
+            //当前显示图片
+            current: imgs[index],
+            //所有图片
+            urls: imgs
+        })
+    },
 
-  upload(){
-    console.log(this.data.phone)
-    console.log(this.data.real_name)
-    console.log(this.data.imgs)
-  },
+    upload() {
+        console.log(this.data.phone)
+        console.log(this.data.real_name)
+        console.log(this.data.imgs)
+    },
 
-
-  clearFont() {
-    this.setData({
-      placeholder: ''
-    })
-  },
+    clearFont() {
+        this.setData({
+            placeholder: ''
+        })
+    },
 
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-  }
+    }
 })
